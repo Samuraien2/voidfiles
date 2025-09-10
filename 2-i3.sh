3d666584da80b2064b588e487ad6cfcbefe6d40f
Samuraien2 <samuelmjacobsen@gmail.com>
2025-09-10 15:38:46 +0200

    changes

--- 2-i3.sh
+++ 2-i3.sh
@@ -4,9 +4,11 @@ mkdir -p ~/doc
 mkdir -p ~/img
 mkdir -p ~/music
 mkdir -p ~/.config/alacritty
+mkdir -p ~/.config/rofi
 
 cp files/i3-config ~/.config/i3/config
 cp files/alacritty.toml ~/.config/alacritty/
 cp files/xinitrc ~/.xinitrc
 cp files/bash_profile ~/.bash_profile
 cp files/user-dirs.dirs ~/.config/
+cp files/rofi.conf ~/.config/rofi/config.rasi
