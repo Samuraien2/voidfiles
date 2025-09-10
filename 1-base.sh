fc3bc37d108a364101b417f754bf8638f523a69f
Samuraien2 <samuelmjacobsen@gmail.com>
2025-09-10 11:09:32 +0200

    changes

--- 1-base.sh
+++ 1-base.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
-REPO_FILE=/etc/xbps.d/00-repository-main.conf
-
 if [[ $EUID -ne 0 ]]; then
     echo "Please run as root."
     exit 1
 fi
 
-cp --preserve=mode,ownership files/rc.local /etc/rc.local
+append() {
+	echo "$1" >> $2
+	echo -e "\e[33m$2 \e[90m+= \e[32m\"$1\"\e[m"
+}
+
+cp "files/rc.local" /etc/rc.local
+
+read -p "rc.local: /run/user/1000? [y/N] " run_user_1000
+if [ "$run_user_1000" = y ]; then
+	append "mkdir -p /run/user/1000" /etc/rc.local
+fi
 
 
 rm -f /etc/xbps.d/*
@@ -14,9 +22,8 @@ rm -f /etc/xbps.d/*
 add_repo() {
 	local url="https://repo-fi.voidlinux.org/current$1"
 	read -p "Add repository $url? [Y/n] " answer
-	if [[ ! "$answer" =~ ^[nN] ]]; then
-		echo "Adding $url to $REPO_FILE"
-		echo "repository=$url" >> $REPO_FILE
+	if [ "$answer" != "n" ]; then
+		append "repository=$url" /etc/xbps.d/00-repository-main.conf
 	fi
 }
 
